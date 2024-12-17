e3cf0850db5bd6ff18060cd4316ca86a42911dc1
Essamaki <contact.essamakiasmae@gmail.com>
2024-12-17 17:29:27 +0100

    google auth url

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -150,7 +150,7 @@ function RegisterForm({ handllSwapForm,handleOpen, redirectRoute }) {
                         )}
                     </Button>
                     <Button color="blue-gray" size="sm" variant="outlined" className="flex items-center justify-center bg-gray-500 text-white hover:bg-gray-800 hover:text-white space-x-2"
-                            onClick={() => (window.location.href = 'http://localhost:8000/auth/google')}>
+                            onClick={() => (window.location.href = 'https://brikool-server-git-main-aymane-moutousses-projects.vercel.app/auth/google')}>
                         <svg className={"h-4 w-4 fill-white"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                             <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
                         </svg>
