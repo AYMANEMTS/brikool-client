7cc62db0ffb2cc903a30dfad428be4d1a789c923
aymane <oujdimraymane@gmail.com>
2024-12-23 09:36:29 +0100

    update home page 2

--- src/components/client/home-page/CategoriesSlider.jsx
+++ src/components/client/home-page/CategoriesSlider.jsx
@@ -75,7 +75,7 @@ function CategoriesSlider({ t }) {
                             key={key}
                             className="bg-gray-300 dark:bg-gray-700 p-5 m-2 rounded-lg text-center shadow-md hover:shadow-lg transition"
                         >
-                            <div className="mb-2 sm:mb-4" onClick={() => navigate(`/workers?cat_id=${item._id}`)}>
+                            <div className="mb-2 sm:mb-4 cursor-pointer" onClick={() => navigate(`/workers?cat_id=${item._id}`)}>
                                 <img src={`https://brikool-server-git-main-aymane-moutousses-projects.vercel.app/${item.image}`} alt={item?.name?.[lng]} className="rounded-md" />
                             </div>
                             <p className="text-teal-blue dark:text-bright-yellow font-semibold text-xs capitalize sm:text-sm">
